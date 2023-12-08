f9b0f872873b39d5a05b1e15ac544a0e6cc9526f
Ramzi <rakhoury1@yahoo.com>
2023-12-08 11:03:43 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({hi: 'there'})
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log("listening on port 4000");
+});
